c885ac166c36d3d412da302382d63d311568206f
Thai Tran <Thai56@Thais-MacBook-Air.local>
2016-12-12 16:36:17 -0700

    complete

--- controller.js
+++ controller.js
@@ -1,5 +1,12 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular.module('userProfiles').controller('MainController', function($scope,mainService){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+  //  $scope.getData = mainService.getUsers();
+   $scope.getData = mainService.getUsers();
+
+
+
+
+   $scope.toggleFavorite = mainService.toggleFavorite;
 })
---
+++ mainService.js
@@ -0,0 +1,36 @@
+angular.module('userProfiles').service('mainService', function($http){
+
+  var friends = [
+    {
+        "id": 0,
+        "first_name": "george",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+    },
+    {
+        "id": 1,
+        "first_name": "lucille",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+    },
+    {
+        "id": 2,
+        "first_name": "oscar",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+    }
+  ]
+
+  this.getUsers = function(){
+    return friends;
+  }
+  // this.isFavorite= false;
+  // this.toggleFavorite = function(userIndex) {
+  //   friends[userIndex].isFavorite = !friends[userIndex].isFavorite;
+  // }
+  this.toggleFavorite = function(userIndex){
+      friends[userIndex].isFavorite = !friends[userIndex].isFavorite;
+  }
+
+
+})
